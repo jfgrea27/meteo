6e7a4cea11f400a92ebf576af14121537fb3e121
James Rea <jfgrea27@gmail.com>
2026-03-07 12:53:14 +0000

    [chore]: Nix flakify repository.
    - Add `.gitignore` with `.direnv`, `.venv`.
    - Add `flake.nix` with Python, K8S, PostgreSQL.
    - Add `.envrc` for hooking up with `direnv`.

---
+++ flake.nix
@@ -0,0 +1,27 @@
+{
+  description = "rt-meteo dev environment";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+    let
+      pkgs = import nixpkgs { inherit system; config = {
+        allowUnfree = true;
+      };};
+    in {
+      devShell = pkgs.mkShell {
+        packages = [
+          pkgs.uv
+          pkgs.postgresql
+          pkgs.kubectl
+          pkgs.kubernetes-helm
+        ];
+        shellHook = ''
+        '';
+      };
+    });
+}
